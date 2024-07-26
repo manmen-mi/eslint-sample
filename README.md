9e95aaadde8bfeff2f205d1dc4e3f0b2af913415
Kim <kim@fb.tapweb.jp>
2024-07-26 10:18:00 +0900

    sample code, command

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # eslint-sample
 
 ```sh
+pnpm add -D pnpm create @eslint/config@latest
+```
+
+```sh
 pnpm add -D eslint globals @eslint/js typescript-eslint@rc-v8
 ```
--- src/counter.ts
+++ src/counter.ts
@@ -1,8 +1,11 @@
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
-  const setCounter = (count: number) => {
+  const setCounter = (count: number)=> {
     counter = count
     element.innerHTML = `count is ${counter}`
+    if(counter===1){
+console.log('counter is 1')
+    }
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
